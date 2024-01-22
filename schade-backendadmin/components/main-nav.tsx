8f56bc6b4ab43fbe1938257ff30bb8ff6fdf7e1f
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-01-22 12:52:59 +0000

    Settings page

--- schade-backendadmin/components/main-nav.tsx
+++ schade-backendadmin/components/main-nav.tsx
@@ -12,10 +12,16 @@ export function MainNav({
     const params = useParams();
 
     const routes = [{
+        href: `/${params.storeId}`,
+        label: 'Dashboard',
+        active: pathname === `/${params.storeId}`,
+    },
+    {
         href: `/${params.storeId}/settings`,
         label: 'Settings',
         active: pathname === `/${params.storeId}/settings`,
-    },];
+    },
+    ];
 
     return(
         <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
