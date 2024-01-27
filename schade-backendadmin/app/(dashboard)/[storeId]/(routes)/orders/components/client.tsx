869972502d6b1d3e620631a38faf88f89c336521
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-01-27 14:59:47 +0000

    Products and Orders Entities

---
+++ schade-backendadmin/app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { useParams, useRouter } from "next/navigation";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface OrderClientProps{
+    data: OrderColumn[],
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+
+    return(
+        <>
+            <Heading title={`Orders (${data.length})`} description="Manage orders for your store"/>
+            <Separator/>
+            <DataTable columns={columns} data={data} searchKey="products"/>
+        </>
+    )
+}
--- schade-backendadmin/components/main-nav.tsx
+++ schade-backendadmin/components/main-nav.tsx
@@ -37,6 +37,16 @@ export function MainNav({
         active: pathname === `/${params.storeId}/colours`,
     },
     {
+        href: `/${params.storeId}/products`,
+        label: 'Products',
+        active: pathname === `/${params.storeId}/products`,
+    },
+    {
+        href: `/${params.storeId}/orders`,
+        label: 'Orders',
+        active: pathname === `/${params.storeId}/orders`,
+    },
+    {
         href: `/${params.storeId}/settings`,
         label: 'Settings',
         active: pathname === `/${params.storeId}/settings`,
