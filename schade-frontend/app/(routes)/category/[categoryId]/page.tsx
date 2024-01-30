fbbbcee087aec423061f6cff20290486fe241b71
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-01-30 16:07:45 +0000

    Product screen and Category screen implemented

---
+++ schade-frontend/app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,63 @@
+import getCategory from "@/actions/get-category";
+import getColours from "@/actions/get-colours";
+import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import Filter from "./components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
+import MobileFilter from "./components/mobile-filter";
+
+export const revalidate = 0;
+
+interface CategoryPageProps{
+    params: {
+        categoryId: string, 
+    },
+    searchParams: {
+        colourId: string,
+        sizeId: string,
+    }
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+    params,
+    searchParams
+}) => {
+    const products = await getProducts({
+        categoryId: params.categoryId,
+        colourId: searchParams.colourId,
+        sizeId: searchParams.sizeId,
+    });
+    const sizes = await getSizes();
+    const colours = await getColours();
+    const category = await getCategory(params.categoryId);
+
+    return(
+        <div className="bg-white">
+            <Container>
+                <Billboard data={category.billboard}/>
+                <div className="px-4 sm:px-6 lg:px-8 pb-24">
+                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                        <MobileFilter sizes={sizes} colours={colours} />
+                        <div className="hidden lg:block">
+                            <Filter valueKey="sizeId" name="Sizes" data={sizes}/>
+                            <Filter valueKey="colourId" name="Colours" data={colours}/>
+                        </div>
+                        <div className="mt-6 lg:col-span-4 lg:mt-0">
+                            {products.length === 0 && <NoResults/>}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                {products.map((item) => (
+                                    <ProductCard key={item.id} data={item}/>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default CategoryPage;
--- schade-frontend/components/ui/product-card.tsx
+++ schade-frontend/components/ui/product-card.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps{
     data: Product;
@@ -13,8 +14,14 @@ interface ProductCardProps{
 const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/product/${data?.id}`);
+    } 
+
     return(
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+        <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Icon */}
             <div className="aspect-square rounded-xl bg-gray-100 relative">
                 <Image src={data?.images?.[0]?.url} alt="Image" fill className="aspect-square object-cover rounded-md"/>
