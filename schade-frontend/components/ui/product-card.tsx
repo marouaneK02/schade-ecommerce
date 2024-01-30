8d781488f150b5cda9c4a69fe5134da2ef789a39
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-01-30 22:45:44 +0000

    Preview product component

--- schade-frontend/components/ui/product-card.tsx
+++ schade-frontend/components/ui/product-card.tsx
@@ -6,6 +6,8 @@ import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps{
     data: Product;
@@ -14,11 +16,18 @@ interface ProductCardProps{
 const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+    const previewModal = usePreviewModal(); 
     const router = useRouter();
 
     const handleClick = () => {
         router.push(`/product/${data?.id}`);
-    } 
+    };
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        previewModal.onOpen(data);
+    }
 
     return(
         <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
@@ -27,7 +36,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
                 <Image src={data?.images?.[0]?.url} alt="Image" fill className="aspect-square object-cover rounded-md"/>
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
-                        <IconButton onClick={() => {}} icon={<Expand size={20} className="text-gray-600"/>}/>
+                        <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600"/>}/>
                         <IconButton onClick={() => {}} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
                     </div>
                 </div>
