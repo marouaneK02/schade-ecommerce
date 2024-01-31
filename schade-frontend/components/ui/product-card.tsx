3d8a19aa3e596be6d5fecb688be0109cdaeb9009
marouaneK02 <152206957+marouaneK02@users.noreply.github.com>
2024-01-31 16:54:52 +0000

    Add to cart component

--- schade-frontend/components/ui/product-card.tsx
+++ schade-frontend/components/ui/product-card.tsx
@@ -8,6 +8,7 @@ import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps{
     data: Product;
@@ -16,6 +17,7 @@ interface ProductCardProps{
 const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+    const cart = useCart();
     const previewModal = usePreviewModal(); 
     const router = useRouter();
 
@@ -29,6 +31,12 @@ const ProductCard: React.FC<ProductCardProps> = ({
         previewModal.onOpen(data);
     }
 
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        cart.addItem(data);
+    }
+
     return(
         <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Icon */}
@@ -37,7 +45,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
                 <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
                     <div className="flex gap-x-6 justify-center">
                         <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600"/>}/>
-                        <IconButton onClick={() => {}} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
+                        <IconButton onClick={onAddToCart} icon={<ShoppingCart size={20} className="text-gray-600"/>}/>
                     </div>
                 </div>
             </div>
